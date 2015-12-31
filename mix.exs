2dbcc1406b57aebabd693d1ea864870b33b72b93
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-12-30 21:40:37 -0800

    Update mix

--- mix.exs
+++ mix.exs
@@ -2,12 +2,25 @@ defmodule Besom.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :besom,
+    [app:     :besom,
+     name:    "Besom",
+
+     description: "Common combinators for Elixir",
+     package: package,
+
      version: "0.0.1",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
+     elixir:  "~> 1.1",
+
+     source_url:   "https://github.com/robot-overlord/besom",
+     homepage_url: "https://github.com/robot-overlord/besom",
+
+     build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+
+     deps: deps,
+     docs: [
+       logo: "./logo.png",
+       extras: ["README.md"]]]
   end
 
   # Configuration for the OTP application
@@ -27,6 +40,15 @@ defmodule Besom.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.10", only: :dev}]
   end
+
+  defp package do
+    [maintainers: ["Brooklyn Zelenka"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/robot-overlord/besom",
+              "Docs" => "http://robot-overlord.github.io/besom/"}]
+  end
+
 end
