ff15b7fa8c83f42c0a9d218885f0920eb45eabf6
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-12-30 23:06:12 -0800

    Start on fixed points

--- lib/besom/classic/fixed_point.ex
+++ lib/besom/classic/fixed_point.ex
@@ -1,4 +1,9 @@
 defmodule Besom.Classic.FixedPoint do
+  @moduledoc ~S"""
+  """
+
+  import Besom.Classix.BCKW, only: [b: 3]
+
   @doc ~S"""
   The famous Y-combinator. This is a [fixed-point](https://en.wikipedia.org/wiki/Fixed-point_combinator),
   which means that applying arguments to this function will always result in a function
@@ -18,4 +23,27 @@ defmodule Besom.Classic.FixedPoint do
   end
 
   defp y_apply(func, x), do: func.(x, x)
+
+  def turing() do
+    Θ = (λx. λy. (y (x x y))) (λx. λy. (y (x x y)))
+    Θv = (λx. λy. (y (λz. x x y z))) (λx. λy. (y (λz. x x y z)))
+  end
+
+  @doc ~S"""
+  A normal order fixed point
+  """
+  @spec z((... -> any), any) :: (... -> any)
+  def z(g, v), do: g.(z(g)).(v)
+
+  @doc ~S"""
+  A strictly non-standard fixed-point combinator
+  """
+  @spec n((... -> any)) :: (... -> any)
+  def n(arg), do: b(m, (b(b(m),b)), arg)
+
+  @doc ~S"""
+  Apply a function to itself
+  """
+  @spec m((... -> any)) :: (... -> any)
+  defp m(fun), do: &fun.(fun.(&1))
 end
