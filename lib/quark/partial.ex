ea9f2390af354e44f8897e627a671d9b3f98c278
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-01-02 10:31:29 -0800

    defpartial working & tested

--- lib/quark/partial.ex
+++ lib/quark/partial.ex
@@ -12,37 +12,50 @@ defmodule Quark.Partial do
 
   defmacro __using__(_) do
     quote do
-      import Quark.Partial, only: [defpartial: 2] #, defpartialp: 2]
+      import Quark.Partial, only: [defpartial: 2, defpartialp: 2]
     end
   end
 
   @doc ~S"""
-  defpartial add(a, b), do: a + b
   """
   defmacro defpartial({fun_name, ctx, args}, do: body) do
-    rehydrate({fun_name, ctx, []}, body, args)
+    Enum.map(args_scan(args), &rehydrate(fun_name, ctx, &1, body, args))
   end
 
-  defp rehydrate({fun_name, ctx, []}, body, args) do
+  defp rehydrate(fun_name, ctx, [], body, full_args) do
     quote do
-      defcurry unquote({fun_name, ctx, args}), do: unquote(body)
+      defcurry unquote({fun_name, ctx, full_args}), do: unquote(body)
     end
   end
 
-  defp rehydrate({fun_name, ctx, args}, body, []) do
-    # quote do
-    #   defcurry unquote({fun_name, ctx, []}), do: unquote(body)
-    # end
+  defp rehydrate(fun_name, ctx, args, _, _) do
+    quote do
+      def unquote({fun_name, ctx, args}), do: unquote(partial_apply(fun_name, args))
+    end
   end
 
-  defp rehydrate({fun_name, ctx, args}, body, next_args) do
-    quote do
-      def unquote({fun_name, ctx, args}) do
-        uncurry(unquote(fun_name)(), unquote(args))
-      end
-    end
+  defmacro defpartialp({fun_name, ctx, args}, do: body) do
+    Enum.map(args_scan(args), &rehydratep(fun_name, ctx, &1, body, args))
+  end
 
-    [na|nas] = next_args
-    rehydrate({fun_name, ctx, [na|args]}, body, nas)
+  defp rehydratep(fun_name, ctx, [], body, full_args) do
+    quote do
+      defcurryp unquote({fun_name, ctx, full_args}), do: unquote(body)
+    end
+  end
+
+  defp rehydratep(fun_name, ctx, args, _, _) do
+    quote do
+      defp unquote({fun_name, ctx, args}), do: unquote(partial_apply(fun_name, args))
+    end
+  end
+
+  defp args_scan(args), do: [[] | Enum.scan(args, [], &(&2 ++ [&1]))]
+
+  defp partial_apply(fun_name, args) do
+    {as, [a]} = Enum.split(args, -1)
+    quote do
+      unquote(fun_name)(unquote_splicing(as)).(unquote(a))
+    end
   end
 end
--- test/quark/partial_test.exs
+++ test/quark/partial_test.exs
@@ -7,8 +7,30 @@ defmodule Quark.PartialTest do
     assert one() == 1
   end
 
-  defpartial div(a, b), do: a / b
+  defpartial minus(a, b, c), do: a - b - c
   test "creates fully-curried functions" do
-    assert div.(10).(2) == 5
+    assert minus.(10).(2).(1) == 7
   end
+
+  test "can partially apply functions" do
+    assert minus(100).(5).(10) == 85
+  end
+
+  test "can use functions in an uncurried manner" do
+    assert minus(4, 2, 1) == 1
+  end
+
+  defpartialp minusp(a, b), do: a - b
+  test "creates fully-curried private functions" do
+    assert minusp.(10).(2) == 8
+  end
+
+  test "can partially apply private functions" do
+    assert minusp(100).(5) == 95
+  end
+
+  test "can use private functions in an uncurried manner" do
+    assert minusp(4, 2) == 2
+  end
+
 end
