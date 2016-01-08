42b5d025b9d5e01b2c96ca88586408a9dac208f8
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-01-07 22:02:04 -0800

    Add Inch dependency

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Quark.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.10", only: :dev}]
+     {:ex_doc, "~> 0.10", only: :dev},
+     {:inch_ex, only: :docs}]
   end
 
   defp package do
