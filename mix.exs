ce0465d565dd32db5d7b9f836ca59ac541952e3a
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-03-13 17:00:58 -0800

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Quark.Mixfile do
       name: "Quark",
 
       description: "Common combinators for Elixir",
-      version: "2.3.0",
+      version: "2.3.1",
       elixir:  "~> 1.11",
 
       package: [
