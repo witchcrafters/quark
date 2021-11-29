386079524cbfcb847d95e2ae577c9a0633577a04
Trevor Lee <trevor@momenti.tv>
2021-11-29 19:00:21 +0900

    Comment for nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 let
   nixpkgs = import (fetchTarball {
+    # For compiled binary, run
+    # `cachix use jechol`
     url = "https://github.com/jechol/nixpkgs/archive/otp24-no-jit.tar.gz";
     sha256 = "sha256:01n9hn9v7w9kgcd4zipf08bg9kskmpm7sp7f8z3yawk2c0w7q2kl";
   }) { };
